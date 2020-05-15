9889ba15751e7b3fd9dc19f84de1945b23f387c4
vsnilov <nilovweb@gmail.com>
2020-05-15 10:36:06 +0300

    pug validation

--- gulp/pug.gulp.js
+++ gulp/pug.gulp.js
@@ -2,19 +2,31 @@ import gulp from 'gulp';
 import notify from 'gulp-notify';
 import pug from 'gulp-pug';
 import fs from 'fs';
+import pugLinter from 'gulp-pug-linter';
+import htmlValidator from 'gulp-w3c-html-validator';
+import bemValidator from 'gulp-html-bem-validator';
 
 gulp.task('pug', () => {
-  return gulp.src(`${pathsSRC.pug}/pages/*.pug`)
-    .pipe(pug({
-      locals: {
-        nav: JSON.parse(fs.readFileSync('./data/navigation.json', 'utf8')),
-        content: JSON.parse(fs.readFileSync('./data/content.json', 'utf8')),
-      },
-      pretty: true
-    }))
-    .on('error', notify.onError({
-      title: "Error in Jade Partials",
-      message: "Error: <%= error.message %>",
-    }))
+  return gulp
+    .src(`${pathsSRC.pug}/pages/*.pug`)
+    .pipe(pugLinter({ reporter: 'default' }))
+    .pipe(
+      pug({
+        locals: {
+          nav: JSON.parse(fs.readFileSync('./data/navigation.json', 'utf8')),
+          content: JSON.parse(fs.readFileSync('./data/content.json', 'utf8')),
+        },
+        pretty: true,
+      })
+    )
+    .pipe(htmlValidator())
+    .pipe(bemValidator())
+    .on(
+      'error',
+      notify.onError({
+        title: 'Error in Jade Partials',
+        message: 'Error: <%= error.message %>',
+      })
+    )
     .pipe(gulp.dest(`${pathsBUILD.html}`));
 });
---
+++ src/static/js/vue/components/Temp.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Привет, Vue',
+    };
+  },
+};
+</script>
+
+<template lang="pug">
+	h1 {{message}}
+</template>
