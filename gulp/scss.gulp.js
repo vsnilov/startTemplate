694d0c94d9682ea2a26e2bca1c863db928fb0021
vsnilov <nilovweb@gmail.com>
2020-08-19 08:22:21 +0300

    scss

--- gulp/scss.gulp.js
+++ gulp/scss.gulp.js
@@ -1,16 +1,16 @@
 import gulp from 'gulp';
-import browserSync from 'browser-sync';
 import gulpIf from 'gulp-if';
+import concat from 'gulp-concat';
 import sass from 'gulp-sass';
 import base64 from 'gulp-css-base64';
 import postcss from 'gulp-postcss';
 import csso from 'gulp-csso';
 import gcmq from 'gulp-group-css-media-queries';
+import sourcemaps from 'gulp-sourcemaps';
 import autoprefixer from 'autoprefixer';
-import concat from 'gulp-concat';
+import browserSync from 'browser-sync';
 import flexbugs from 'postcss-flexbugs-fixes';
 import animation from 'postcss-animation';
-import gulpSourcemaps from 'gulp-sourcemaps'
 
 const isDebug = process.env.NODE_ENV !== 'production';
 const processors = [flexbugs, animation, autoprefixer];
@@ -24,14 +24,14 @@ gulp.task('scss', () =>
     `${global.pathsSRC.scss}fonts.scss`,
     `${global.pathsSRC.scss}**/*.scss`
   ])
-  .pipe(gulpIf(isDebug, gulpSourcemaps.init()))
+  .pipe(gulpIf(isDebug, sourcemaps.init()))
   .pipe(concat('style.scss'))
   .pipe(
     sass({
       includePaths: ['node_modules'],
     })
   )
-  .pipe(gulpIf(isDebug, gulpSourcemaps.write()))
+  .pipe(gulpIf(isDebug, sourcemaps.write()))
   .pipe(gulpIf(!isDebug, gcmq()))
   .pipe(gulpIf(!isDebug, postcss(processors)))
   .pipe(
